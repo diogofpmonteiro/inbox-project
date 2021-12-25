5156a9845907c6ba5d52729cd83f5c80681739b8
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2021-12-25 19:41:45 +0000

    create readme and test files

---
+++ README.md
@@ -0,0 +1,1 @@
+Small project to learn the basics of smart contracts and testing with mocha
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,3 +1,6 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
+
+// Creates instance of web3 and tells it to connect to the provider (ganache, in this case)
+const web3 = new Web3(ganache.provider());
